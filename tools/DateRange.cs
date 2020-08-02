1320a7046f1ce51f10fc421fc9e0122e03ffc0e0
Charles Anderson <hpcngmoh@gmail.com>
2020-08-01 21:38:42 -0400

    tt

--- tools/DateRange.cs
+++ tools/DateRange.cs
@@ -6,8 +6,8 @@ namespace reddit_scraper.Tools
     {
         public DateTime After { get; set; }
         public DateTime Before { get; set; }
-        public static double TotalSecondsFromEpoch(DateTime date) =>
-            date.Subtract(new DateTime(1970, 1, 1, 0, 0, 0, 0)).TotalSeconds;
+        public static long TotalSecondsFromEpoch(DateTime date) =>
+            (long)date.Subtract(new DateTime(1970, 1, 1, 0, 0, 0, 0)).TotalSeconds;
         public static DateTime UnixTimeStampToDateTime(double unixTimeStamp) =>
              new DateTime(1970, 1, 1, 0, 0, 0, 0, DateTimeKind.Utc)
                 .AddSeconds(unixTimeStamp);
